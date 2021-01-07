1b47c97e5003706d6defe2108c4080f6f751bdb5
JohnBosco <johnmatanda@cleverapps.se>
2021-01-07 12:16:44 +0100

    Dynamic Theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,63 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive/hive.dart';
+import 'Pages/setThemePage.dart';
+import 'home/home.dart';
+import 'model_providers/theme_provider.dart'; 
 import 'nav/nav.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+import 'package:provider/provider.dart';
+import 'package:path_provider/path_provider.dart' as pathProvider;
 
-void main() {
-  runApp(MyApp());
+
+void main() async{
+    WidgetsFlutterBinding.ensureInitialized();
+  final appDocumentDirectory =
+      await pathProvider.getApplicationDocumentsDirectory();
+
+  Hive.init(appDocumentDirectory.path);
+
+  final settings = await Hive.openBox('settings');
+  bool isLightTheme = settings.get('isLightTheme') ?? false;
+
+  print(isLightTheme);
+
+  runApp(ChangeNotifierProvider(
+    create: (_) => ThemeProvider(isLightTheme: isLightTheme),
+    child: AppStart(),
+  ));
+
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+
+class AppStart extends StatelessWidget {
+  const AppStart({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    ThemeProvider themeProvider = Provider.of<ThemeProvider>(context);
+    return MyApp(
+      themeProvider: themeProvider,
+    );
+  }
+}
+
+class MyApp extends StatefulWidget with WidgetsBindingObserver {
+  final ThemeProvider themeProvider;
+
+  const MyApp({Key key, @required this.themeProvider}) : super(key: key);
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: NavPage(),  
+      title: 'Light Dark Theme',
+      theme: widget.themeProvider.themeData(),
+      home: SetThemePage(),
     );
   }
 }
